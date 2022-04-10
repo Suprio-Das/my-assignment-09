52c6ccac78aec4d4d0d7a43bcc7a01d74586115b
Suprio-Das <priyamd507@gmail.com>
2022-04-10 22:19:59 +0600

    menu re-styled

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import CustomLink from '../CustomLink/CustomLink';
 const Header = () => {
     return (
-        <header className='flex justify-center md:justify-between items-center sticky md:px-20 md:py-5  bg-[#0c0c0c] py-5 px-5 '>
+        <header className='flex justify-center md:justify-between items-center sticky md:px-20 md:py-5  bg-teal-500 py-5 px-5 '>
             <div className='hidden md:block md:text-3xl text-white font-bold '>
                 <Link to='./' >Car Marts</Link>
             </div>
