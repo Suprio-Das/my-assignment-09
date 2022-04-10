264ed640094c64bf11b09902abb53118cd49ff4f
Suprio-Das <priyamd507@gmail.com>
2022-04-10 21:15:12 +0600

    review cart added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header className='flex justify-center md:justify-between items-center sticky md:px-20 md:py-5  bg-[#0c0c0c] py-5 px-5 '>
             <div className='hidden md:block md:text-3xl text-white font-bold '>
-                <Link to='./' >Car Store</Link>
+                <Link to='./' >Car Marts</Link>
             </div>
             <nav className='flex md:gap-4 md:text-xl gap-2'>
                 <CustomLink to='/'>Home</CustomLink>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import useCar from '../../usehooks/useCar';
-import p1 from '../../img/2.png'
+import p1 from '../../img/top-car.png'
 import HomeReview from '../HomeReview/HomeReview';
  const Home = () => {
      //hooks
@@ -10,19 +10,19 @@ import HomeReview from '../HomeReview/HomeReview';
 
     return (
         <div>
-            <div className='grid md:grid-cols-2  justify-items-center px-4 py-5 bg-[#040869] width-full'>
+            <div className='grid md:grid-cols-2  justify-items-center align-items-center px-4 py-5 bg-[#bbd05d] width-full'>
                 <div className='md:order-2 order-1'>
-                    <h1 className='md:text-[30px] text-xl text-white  mb-10'>You are your Choices</h1>
-                    <p className='text-white mb-3'>Life is a struggle for single father Chris Gardner. Evicted from their apartment, he and his young son find themselves alone with no place to go. Even though Chris eventually lands a job as an intern at a prestigious brokerage firm, the position pays no money. The pair must live in shelters and endure many hardships, but Chris refuses to give in to despair as he struggles to create a better life for himself and his son</p>
+                    <h1 className='md:text-[30px] text-xl text-white  mb-10'>Shift to success mode.</h1>
+                    <p className='text-white mb-3'>A Car is a essential things for every person. . We all have one dream to buy our dream car. A Car can change your daily life and also add more styles to your personality. Do you want to find out best car which will be the best for you? Then, here you are. We have the best collections of modern cars. You can find your favourite car from us with the reasonable price. So, why are you waiting? Check out our latests collections and be the owner of your dream car.</p>
                     <Link to='/books' type="button" className="py-3 px-5 mr-2 mb-2 text-sm font-medium text-white focus:outline-none bg-lime-600 rounded-full border border-gray-200">Latest Collection</Link>
                 </div>
-                <div className='border-2 px-1 py-1 rounded-lg md:order-1 order-2'>
-                    <img className='rounded-lg' src={p1} alt="" />
+                <div className=' px-1 py-1 rounded-lg md:order-1 order-2'>
+                    <img className='rounded-lg w-72' src={p1} alt="" />
                 </div>
             </div>
-            <div className='bg-[#2e54c5] py-5'>
+            <div className='bg-[#f9305f] py-5'>
                 <h1 className='text-4xl text-white'>Customer Reviews</h1>
-                <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10 border-2 px-3 py-3 rounded'>
+                <div className='grid md:grid-cols-3 justify-center items-center w-[70%] mx-auto gap-5 my-10  px-3 py-3 rounded'>
                     {
                         getCar.map(car => <HomeReview
                             car={car}
