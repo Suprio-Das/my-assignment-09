99a0c956685d8210bd3547c024522f70fced7185
Suprio-Das <priyamd507@gmail.com>
2022-04-10 22:34:15 +0600

    about section added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <p>this is about!!!!!</p>
+        <div className='text-center w-[80%] mx-auto mt-5'>
+            <h1 className='text-3xl'>About Us</h1>
+            <p className='w-[50%] mx-auto text-justify mt-3'>Hey, Welcome to our website. We're the best car dealer. We have thousands of happy customers. We always try to provide the best services to our customers. We contain best of best collections following the trendings. We give our best to staisfy our customers. We always take care of our customers feedback to imporve our services.</p>
         </div>
     );
 };
